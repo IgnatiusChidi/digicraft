dd5d5d10f452692abe31bc7cff6ad4cb9a855770
IgnatiusChidi <138877827+IgnatiusChidi@users.noreply.github.com>
2023-11-10 15:39:07 +0100

    Update Services.jsx

--- src/Services.jsx
+++ src/Services.jsx
@@ -8,15 +8,15 @@ function Services() {
         </div>
         <div id='serviceCardDiv'>
             <div className='serviceCard' id='serviceCard1'>
-                <img src='src\assets\undraw_web_development_0l6v.svg'/>
+                <img src='./assets/undraw_web_development_0l6v.svg'/>
                 <h3>Web Development</h3>
             </div>
             <div className='serviceCard' id='serviceCard2'>
-                <img src='src\assets\undraw_search_re_x5gq.svg' />
+                <img src='./assets/undraw_search_re_x5gq.svg' />
                 <h3>Search Engine<br/>Optimization</h3>
             </div>
             <div className='serviceCard' id='serviceCard3'>
-                <img src='src\assets\undraw_mobile_marketing_re_p77p.svg' />
+                <img src='./assets/undraw_mobile_marketing_re_p77p.svg' />
                 <h3>Social Media<br/>Marketing</h3>
             </div>
         </div>
