46d48bdaa7ea9998c60d185259340606e2789f1f
IgnatiusChidi <138877827+IgnatiusChidi@users.noreply.github.com>
2023-11-10 15:53:44 +0100

    Update Services.jsx

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import img1 from './assets/undraw_web_development_0l6v.svg'
+import img2 from './assets/undraw_search_re_x5gq.svg'
+import img3 from './assets/undraw_mobile_marketing_re_p77p.svg'
+
 
 function Services() {
   return (
@@ -8,15 +12,15 @@ function Services() {
         </div>
         <div id='serviceCardDiv'>
             <div className='serviceCard' id='serviceCard1'>
-                <img src='./assets/undraw_web_development_0l6v.svg'/>
+                <img src={img1}/>
                 <h3>Web Development</h3>
             </div>
             <div className='serviceCard' id='serviceCard2'>
-                <img src='./assets/undraw_search_re_x5gq.svg' />
+                <img src={img2} />
                 <h3>Search Engine<br/>Optimization</h3>
             </div>
             <div className='serviceCard' id='serviceCard3'>
-                <img src='./assets/undraw_mobile_marketing_re_p77p.svg' />
+                <img src={img3} />
                 <h3>Social Media<br/>Marketing</h3>
             </div>
         </div>
